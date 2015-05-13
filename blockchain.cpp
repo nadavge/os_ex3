5195a2d1abff37d5b56c8d4a87b60d78bafe4e37
Nadav <nadav1994@gmail.com>
2015-05-13 15:51:35 +0300

    Add documentation for blockchain, fix spacing
    Still left to add docs to other files (Block.*)

--- blockchain.cpp
+++ blockchain.cpp
@@ -1,4 +1,11 @@
-// TODO add file documentation header
+/*
+ * blockchain.cpp
+ * A bitcoin-inspired multithreaded blockchain implementation
+ * Authors:
+ * 	Nadav Geva <nadav.geva4@gmail.com>
+ * 	Daniel Danon <danondaniel@gmail.com>
+ */
+
 #include <deque>
 #include<pthread.h>
 #include <algorithm>
@@ -7,20 +14,11 @@
 #include "Block.h"
 #include "blockchain.h"
 #include "hash.h"
-// TODO \forall functions check when -1
 // TODO Srand
-// TODO think
-/*
- * TODO daemon might be switched into after close while adding a new block
- * need to *not* do this
- */
 
 using namespace std;
 
-/*
- *       A multi threaded blockchain database manager
- *       Author: we!
- */
+//========================MACROS=================================
 
 #define BLOCK_DOESNT_EXIST -2
 #define NOT_STARTED -1
@@ -32,20 +30,13 @@ using namespace std;
 
 #define compare_and_swap __sync_bool_compare_and_swap
 #define RUNNING() (gBlocksAdded != NOT_STARTED && ! gIsClosing)
+// Tries locking the mutex, returns true iff no errors occured
 #define LOCK_ALL() (pthread_mutex_lock(&lock) == 0)
+// Tries unlocking the mutex, returns true iff no errors occured
 #define UNLOCK_ALL() (pthread_mutex_unlock(&lock) == 0)
 
-#define DEBUG(x)  ////cout << x <<endl
 //========================DECLARATIONS===========================
-int init_blockchain();
-int add_block(char *data , int length);
-int to_longest(int block_num);
-int attach_now(int block_num);
-int was_added(int block_num);
-int chain_size();
-int prune_chain();
-void close_chain();
-int return_on_close();
+
 int takeMinUnusedBlocknum(Block* block);
 Block* getBlockByBlocknum(int blocknum);
 Block* getDeepestBlock();
@@ -58,42 +49,45 @@ void daemonAddBlock(Block* toAdd);
 void terminateDaemon();
 
 //========================GLOBALS================================
+
+// The queue of blocks waiting to be inserted
 static deque<Block*> gQueueBlock;
+// Counter of blockes added to the CHAIN (not CREATED). def: -1
 static int gBlocksAdded = NOT_STARTED;
+// The genesis block
 static Block* gGenesis = nullptr;
+/*
+ * The vector containing all blocks allocated who own an ID.
+ * The index of each block in the vector matches its ID.
+ */
 static vector<Block*> gBlockVector;
+// The vector of all blocks at the depth Blocks::s_maxDepth
 static vector<Block*> gDeepestBlocks;
 static bool gIsClosing = false;
 static pthread_t gDaemonThread = {0};
+// The mutex to allow editing of the chain and blocks structure
 pthread_mutex_t lock;
 
 //========================IMPLEMENTATION==========================
 
-/*
- * DESCRIPTION: This function initiates the Block chain, and creates the genesis Block.  The genesis Block does not hold any transaction data
- *      or hash. should be called prior to any other functions as a necessary precondition for their success (all other functions should
- *      return with an error otherwise).
- * RETURN VALUE: On success 0, otherwise -1.
- */
-
 int init_blockchain()
 {
 	// TODO check flaging system - might not act as intended
-    if(RUNNING())
-    {
-        return ERROR;
-    }
+	if(RUNNING())
+	{
+		return ERROR;
+	}
 	
 	Block::initMaxDepth();
-    gGenesis = new Block();
-    gGenesis->setFather(nullptr);
+	gGenesis = new Block();
+	gGenesis->setFather(nullptr);
 	gGenesis->setDepth(0);
-    gGenesis->setId(0);
-    gBlockVector.push_back(gGenesis);
-    gDeepestBlocks.push_back(gGenesis);
+	gGenesis->setId(0);
+	gBlockVector.push_back(gGenesis);
+	gDeepestBlocks.push_back(gGenesis);
 
-    gBlocksAdded = 0;
-    gIsClosing = false;
+	gBlocksAdded = 0;
+	gIsClosing = false;
 	
 	if (! initDaemon())
 	{
@@ -101,200 +95,180 @@ int init_blockchain()
 		return ERROR;
 	}
 
-    return 0;
+	return 0;
 }
 
-/*
- * DESCRIPTION: Ultimately, the function adds the hash of the data to the Block chain.
- *      Since this is a non-blocking package, your implemented method should return as soon as possible, even before the Block was actually
- *      attached to the chain.
- *      Furthermore, the father Block should be determined before this function returns. The father Block should be the last Block of the
- *      current longest chain (arbitrary longest chain if there is more than onSTARTED()e).
- *      Notice that once this call returns, the original data may be freed by the caller.
- * RETURN VALUE: On success, the function returns the lowest available block_num (> 0),
- *      which is assigned from now on to this individual piece of data.
- *      On failure, -1 will be returned.
- */
 int add_block(char *data , int length)
 {
-    if(! RUNNING())
-    {
-        return ERROR;
-    }
+	if(! RUNNING())
+	{
+		return ERROR;
+	}
 
-    int blockNum = -1;
-    Block* block = new Block();
-    if(! block->setData(data, length))
+	int blockNum = -1;
+	Block* block = new Block();
+
+	if(! block->setData(data, length))
 	{
 		delete block;
 		return ERROR;
 	}
-    block->setFather(getDeepestBlock());
-    blockNum = takeMinUnusedBlocknum(block);
-    addToQueue(block);
-    return blockNum;
+
+	block->setFather(getDeepestBlock());
+	blockNum = takeMinUnusedBlocknum(block);
+	addToQueue(block);
+
+	return blockNum;
 }
 
-/*
- * DESCRIPTION: Without blocking, enforce the policy that this block_num should be attached to the longest chain at the time of attachment of
- *      the Block. For clearance, this is opposed to the original add_block that adds the Block to the longest chain during the time that
- *      add_block was called.
- *      The block_num is the assigned value that was previously returned by add_block.
- * RETURN VALUE: If block_num doesn't exist, return -2; In case of other errors, return -1; In case of success return 0; In case block_num is
- *      already attached return 1.
-*/
 int to_longest(int block_num)
 {
-    if(! RUNNING())
-    {
-        return ERROR;
-    }
+	if(! RUNNING())
+	{
+		return ERROR;
+	}
 
-    Block* block = getBlockByBlocknum(block_num);
+	Block* block = getBlockByBlocknum(block_num);
 
-    if(block == nullptr)
-    {
-        return BLOCK_DOESNT_EXIST;
-    }
-    if(block->inChain())
-    {
-        return BLOCK_IN_CHAIN;
-    }
-    block->setAddInRealTime();
-    if(! block->inChain() || block->wasAddedInRealTime())
-    {
-        return SUCCESS;
-    }
-    else
-    {
-        return BLOCK_IN_CHAIN;
-    }
+	if(block == nullptr)
+	{
+		return BLOCK_DOESNT_EXIST;
+	}
+
+	if(block->inChain())
+	{
+		return BLOCK_IN_CHAIN;
+	}
+
+	block->setAddInRealTime();
+
+	if(! block->inChain() || block->wasAddedInRealTime())
+	{
+		return SUCCESS;
+	}
+	else
+	{
+		return BLOCK_IN_CHAIN;
+	}
 }
 
-/*
- * DESCRIPTION: This function blocks all other Block attachments, until block_num is added to the chain. The block_num is the assigned value
- *      that was previously returned by add_block.
- * RETURN VALUE: If block_num doesn't exist, return -2;
- *      In case of other errors, return -1; In case of success or if it is already attached return 0.
-*/
 int attach_now(int block_num)
 {
-    if(! RUNNING())
-    {
-        return ERROR;
-    }
-    if(! LOCK_ALL())
-    {
-        return ERROR;
-    }
-    Block* block = getBlockByBlocknum(block_num);
-    if(block == nullptr)
-    {
-	if(! UNLOCK_ALL())
+	if(! RUNNING())
 	{
 		return ERROR;
 	}
-        return BLOCK_DOESNT_EXIST;
-    }
-    if(! block->inChain())
-    {
-        addBlockAssumeMutex(block);
-    }
+
+	if(! LOCK_ALL())
+	{
+		return ERROR;
+	}
+
+	Block* block = getBlockByBlocknum(block_num);
+
+	if(block == nullptr)
+	{
+
+		if(! UNLOCK_ALL())
+		{
+			return ERROR;
+		}
+		return BLOCK_DOESNT_EXIST;
+	}
+   
+	if(! block->inChain())
+	{
+		addBlockAssumeMutex(block);
+	}
 
 	if(! UNLOCK_ALL())
 	{
 		return ERROR;
 	}
 
-    return SUCCESS;
+	return SUCCESS;
 }
-/*
- * DESCRIPTION: Without blocking, check whether block_num was added to the chain.
- *      The block_num is the assigned value that was previously returned by add_block.
- * RETURN VALUE: 1 if true and 0 if false. If the block_num doesn't exist, return -2; In case of other errors, return -1.
-*/
+
 int was_added(int block_num)
 {
-    if(! RUNNING())
-    {
-        return ERROR;
-    }
-    Block* block = getBlockByBlocknum(block_num);
+	if(! RUNNING())
+	{
+		return ERROR;
+	}
 
-    if(block == nullptr)
-    {
-        return BLOCK_DOESNT_EXIST;
-    }
+	Block* block = getBlockByBlocknum(block_num);
 
-    if(block->inChain())
-    {
-        return BLOCK_IN_CHAIN;
-    }
-    else
-    {
-        return BLOCK_NOT_IN_CHAIN; 
-    }
+	if(block == nullptr)
+	{
+		return BLOCK_DOESNT_EXIST;
+	}
+
+	if(block->inChain())
+	{
+		return BLOCK_IN_CHAIN;
+	}
+
+	else
+	{
+		return BLOCK_NOT_IN_CHAIN; 
+	}
 }
 
-/*
- * DESCRIPTION: Return how many Blocks were attached to the chain since init_blockchain.
- *      If the chain was closed (by using close_chain) and then initialized (init_blockchain) again this function should return
- *      the new chain size.
- * RETURN VALUE: On success, the number of Blocks, otherwise -1.
-*/
 int chain_size()
 {
-    if(! RUNNING())
-    {
-        return ERROR;
-    }
-    return gBlocksAdded;
+	if(! RUNNING())
+	{
+		return ERROR;
+	}
+
+	return gBlocksAdded;
 }
 
-/*
- * DESCRIPTION: Search throughout the tree for sub-chains that are not the longest chain,
- *      detach them from the tree, free the blocks, and reuse the block_nums.
- * RETURN VALUE: On success 0, otherwise -1.
-*/
 int prune_chain()
 {
-    if(! RUNNING())
-    {
-        return ERROR;
-    }
-    if(! LOCK_ALL())
-    {
-        return ERROR;
-    }
+	/*
+	 * The mechanism is as such:
+	 * 1. Get a block from the deepest depth, and build its route to genesis in an array
+	 * 2. Iterate over all allocated blocks, and for each block that is in the block chain but
+	 *	not in the longest chain chosen, delete it from the chain and free it
+	 * 3. Iterate over all blocks waiting to be added, and for each of them whose father was
+	 *	deleted, choose a new father who's the deepest block in the current chain
+	 */
+	if(! RUNNING())
+	{
+		return ERROR;
+	}
 
-	//cout << "In prune" << endl;
-    Block* deepest = getDeepestBlock();
-	//cout << "got deepest" << endl;
-    Block* longestChain[Block::getMaxDepth() + 1];
-    while(deepest != nullptr)
-    {
-	//cout << "Currently at " << deepest->getDepth() << "/" << Block::getMaxDepth() << endl;
-        longestChain[deepest->getDepth()] = deepest;
-        deepest = deepest->getFather();
-    }
-	//cout << "found root" << endl;
-    for(unsigned int i = 0; i < gBlockVector.size(); i++)
-    {
-        Block* currBlock;
-        currBlock = gBlockVector.at(i);
-        if(currBlock == nullptr || !currBlock->inChain())
-        {
-            continue;
-        }
+	if(! LOCK_ALL())
+	{
+		return ERROR;
+	}
 
-        if(currBlock != longestChain[currBlock->getDepth()])
-        {
-            delete currBlock;
-            gBlockVector.at(i) = nullptr;
-        }
-    }
+	Block* deepest = getDeepestBlock();
+	Block* longestChain[Block::getMaxDepth() + 1];
+	
+	while(deepest != nullptr)
+	{
+		longestChain[deepest->getDepth()] = deepest;
+		deepest = deepest->getFather();
+	}
+   
+	for(unsigned int i = 0; i < gBlockVector.size(); i++)
+	{
+		Block* currBlock;
+		currBlock = gBlockVector.at(i);
+		if(currBlock == nullptr || !currBlock->inChain())
+		{
+			continue;
+		}
 
-	//cout << "Cleared non queued + chain blocks" << endl;
+		if(currBlock != longestChain[currBlock->getDepth()])
+		{
+			gBlockVector.at(i) = nullptr;
+			delete currBlock;
+		}
+	}
+
 	for (auto &block : gQueueBlock)
 	{
 		/*
@@ -320,75 +294,63 @@ int prune_chain()
 	gDeepestBlocks.clear();
 	gDeepestBlocks.push_back(longestChain[Block::getMaxDepth()]);
 
-    if (! UNLOCK_ALL())
-    {
-        return ERROR;
-    }
+	if (! UNLOCK_ALL())
+	{
+		return ERROR;
+	}
 
 	return SUCCESS;
 }
 
-/*
- * DESCRIPTION: Close the recent blockchain and reset the system, so that it is possible to call init_blockchain again. Non-blocking.
- *      All pending Blocks should be hashed and printed to terminal (stdout).
- *      Calls to library methods which try to alter the state of the Blockchain are prohibited while closing the Blockchain. e.g.: Calling
- *      chain_size() is ok, a call to prune_chain() should fail.
- *      In case of a system error, the function should cause the process to exit.
-*/
 void close_chain()
 {
-    if(! RUNNING())
-    {
-        exit(2);
-    }
+	if(! RUNNING())
+	{
+		exit(2);
+	}
 
-    gIsClosing = true;
-
+	gIsClosing = true;
 }
-
-/*
- * DESCRIPTION: The function blocks and waits for close_chain to finish.
- * RETURN VALUE: If closing was successful, it returns 0.
- *      If close_chain was not called it should return -2. In case of other error, it should return -1.
-*/ //========================GLOBALS================================
-
 
 int return_on_close()
 {
-    if( ! gIsClosing && gBlocksAdded != NOT_STARTED)
-    {
-        return NOT_CLOSING;
-    }
-	pthread_join(gDaemonThread, nullptr);
-//TODO remove if pthread_join works
-/*    while(gIsClosing)
-    {
-        // TODO Maybe let pthread know we're waiting?
-    }
-*/
+	if( ! gIsClosing && gBlocksAdded != NOT_STARTED)
+	{
+		return NOT_CLOSING;
+	}
 
-    return 0;
+	pthread_join(gDaemonThread, nullptr);
+	
+	return 0;
 }
 
 
+/**
+* @brief Takes the minimum unused block num available
+*
+* @param block the block to attach to the min num
+*
+* @return the block num taken
+*/
 int takeMinUnusedBlocknum(Block* block)
 {
 	// Signals whether the function got the min value
 	bool tookMin = false;
-    unsigned int blocknum = 0;
+	unsigned int blocknum = 0;
 
+	// Until took the min or found out that no free id's are, try and take the minimum
 	while (! tookMin && blocknum < gBlockVector.size())
 	{
 		blocknum = 0;
    		for (auto &currBlock : gBlockVector)
-    	{
-       		if(currBlock == nullptr)
-        	{
+		{
+	   		if(currBlock == nullptr)
+			{
 				tookMin = compare_and_swap(&gBlockVector[blocknum], nullptr, block);
 				break;
-        	}
+			}
 
-	        blocknum++;
+			blocknum++;
    		}
 	}
 
@@ -402,8 +364,8 @@ int takeMinUnusedBlocknum(Block* block)
 	{
 
 		blocknum = 0;
-	    gBlockVector.push_back(block);
-	    for (auto &currBlock : gBlockVector)
+		gBlockVector.push_back(block);
+		for (auto &currBlock : gBlockVector)
 		{
 
 			if(currBlock == block)
@@ -414,16 +376,24 @@ int takeMinUnusedBlocknum(Block* block)
 		}
 	}
 
-    return blocknum;
+	return blocknum;
 }
 
+/**
+* @brief Get the block owning the block num
+*
+* @param blocknum the block num
+*
+* @return the block who's block num is the one provided. nullptr if non-existent
+*/
 Block* getBlockByBlocknum(int blocknum)
 {
-    if((unsigned int) blocknum >= gBlockVector.size())
-    {
-        return nullptr;
-    }
-    return gBlockVector.at(blocknum);
+	if((unsigned int) blocknum >= gBlockVector.size())
+	{
+		return nullptr;
+	}
+
+	return gBlockVector.at(blocknum);
 }
 
 /*
@@ -433,11 +403,10 @@ Block* getBlockByBlocknum(int blocknum)
 Block* getDeepestBlock()
 {
 
-    auto it = gDeepestBlocks.begin();
-DEBUG("DBSIZE: "<<gDeepestBlocks.size());
-    advance(it, rand() % gDeepestBlocks.size());
+	auto it = gDeepestBlocks.begin();
+	advance(it, rand() % gDeepestBlocks.size());
 
-    return *it;
+	return *it;
 }
 
 /**
@@ -454,12 +423,18 @@ char* generateHash(Block* block)
 }
 
 //========================DAEMON CODE============================
+
+/**
+* @brief Initialize the demon
+*
+* @return true iff the initialization was successful
+*/
 bool initDaemon()
 {
-    if (pthread_mutex_init(&lock, NULL) != 0)
-    {
-        return false;
-    }
+	if (pthread_mutex_init(&lock, NULL) != 0)
+	{
+		return false;
+	}
 	
 	init_hash_generator();
 	
@@ -473,6 +448,9 @@ bool initDaemon()
 	return true;
 }
 
+/**
+* @brief The daemon thread entry-point
+*/
 void* runDaemon(void* arg)
 {
 	Block* block = nullptr;
@@ -496,12 +474,15 @@ void* runDaemon(void* arg)
 	return nullptr;
 }
 
+/**
+* @brief The daemon termination function
+* Responsible for all the closings
+*/
 void terminateDaemon()
 {
 	char* hash = nullptr;
 	Block* block = nullptr;
 
-	// TODO Free all block items (from everywhere!!!!)
 	if (! LOCK_ALL())
 	{
 		exit(3);
@@ -512,7 +493,6 @@ void terminateDaemon()
 		block = gQueueBlock.front();
 		gQueueBlock.pop_front();
 
-		//cout << "DELETING FROM QUEUE: " << block->getId() << endl;
 		gBlockVector.at(block->getId()) = nullptr;
 
 		hash = generateHash(block);
@@ -525,14 +505,12 @@ void terminateDaemon()
 	{
 		if (block != nullptr)
 		{
-			DEBUG("Deleting " << block->getId());
 			delete block;
 		}
 	}
 
 	gBlockVector.clear();
 	gDeepestBlocks.clear();
-DEBUG("Cleared gDeepestBlock on terminate");
 
 	if (! UNLOCK_ALL())
 	{
@@ -546,34 +524,49 @@ DEBUG("Cleared gDeepestBlock on terminate");
 	gIsClosing = false;
 }
 
+/**
+* @brief Add the block to the pending queue for insertion
+*
+* @param toAdd the block to add
+*/
 inline void addToQueue(Block* toAdd)
 {
-    gQueueBlock.push_back(toAdd);
+	gQueueBlock.push_back(toAdd);
 }
 
 
+/**
+* @brief The wrapper function for daemon adding, responsible for locking
+*
+* @param toAdd the block to add
+*/
 void daemonAddBlock(Block* toAdd)
 {
-	// TODO What if THIS function FAILS?
-    if(! LOCK_ALL())
-    {
+	if(! LOCK_ALL())
+	{
 		exit(6);
-    }
+	}
 
 	addBlockAssumeMutex(toAdd);
 
-    if(! UNLOCK_ALL())
-    {
+	if(! UNLOCK_ALL())
+	{
  		exit(7);
-    }
+	}
 }
+
+/**
+* @brief The add block function for the daemon. Requires locking,
+* but assumes to be called while the mutex is locked by the calling thread.
+*
+* @param toAdd the block to add
+*/
 void addBlockAssumeMutex(Block* toAdd)
 {
-	// TODO What if THIS function FAILS?
-    if(toAdd->toAddInRealTime())
-    {
-        toAdd->setFather(getDeepestBlock());
-    }
+	if(toAdd->toAddInRealTime())
+	{
+		toAdd->setFather(getDeepestBlock());
+	}
 
 	toAdd->setHash(generateHash(toAdd));
 	toAdd->setDepth(toAdd->getFather()->getDepth()+1);
@@ -583,15 +576,14 @@ void addBlockAssumeMutex(Block* toAdd)
 		vector<Block*> tempDeepestBlocks = {toAdd};
 		gDeepestBlocks = tempDeepestBlocks;
 		Block::setMaxDepth(toAdd->getDepth());
-		DEBUG("Got deeper, clearing deepestblock");
 	}
 	else if (toAdd->getDepth() == Block::getMaxDepth())
 	{
-		DEBUG("Adding to deepest block");
 		gDeepestBlocks.push_back(toAdd);
 	}
+	
 	++gBlocksAdded;	
-	// Remove block from pending queue
+	
 	gQueueBlock.erase(remove(gQueueBlock.begin(), gQueueBlock.end(), toAdd), gQueueBlock.end());
 }
 
